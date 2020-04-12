a1b305e71d02b538ce283ee630084a83ee1cb79c
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-04-12 12:05:39 -0400

    Adding interview practice questions

---
+++ Interview_Practice_Question.py
@@ -0,0 +1,306 @@
+#4/2/20
+#You are given two linked-lists representing two
+#non-negative integers. The digits are stored in
+#reverse order and each of their nodes contain a
+#single digit. Add the two numbers and return it as a linked list.
+#Input: (2-> 4 ->3) + (5 -> 6 -> 4)
+#Output: 7 -> 0 -> 8
+#Explanation: 342 + 465 = 807
+
+
+# Definition for singly-linked list.
+'''
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+  def addTwoNumbers(self, l1, l2, c = 0):
+# Fill this in.
+
+result = ListNode(0)
+result_tail = result
+
+while l1 or l2 or c:
+    val1 = (l1.val if l1 else 0)
+    val2 = (l2.val if l2 else 0)
+    c, out = divmod(val1 + val2 + c, 10)
+
+    result_tail.next = ListNode(out)
+    result_tail = result_tail.next
+
+    l1 = (l1.next if l1 else None)
+    l2 = (l2.next if l2 else None)
+
+return result
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(l1, l2)
+
+while result:
+    print(result.val)
+    result = result.next
+# 7 0 8
+'''
+
+#Given a string, find the length of the longest substring
+# without repeating characters. 4/3/20
+
+'''
+class Solution:
+  def lengthOfLongestSubstring(self, s):
+    # Fill this in.
+
+print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+# 10
+'''
+
+#A palindrome is a sequence of characters that reads
+# the same backwards and forwards. Given a string, s,
+# find the longest palindromic substring in s. 4/4/20
+
+'''
+class Solution: 
+    def longestPalindrome(self, s):
+      # Fill this in.
+        
+# Test program
+s = "tracecars"
+print(str(Solution().longestPalindrome(s)))
+# racecar
+'''
+
+#Imagine you are building a compiler. Before running any code,
+# the compiler must check that the parentheses in the program
+# are balanced. Every opening bracket must have a corresponding
+# closing bracket. We can approximate this using strings.
+#Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#An input string is valid if:
+#- Open brackets are closed by the same type of brackets.
+#- Open brackets are closed in the correct order.
+#- Note that an empty string is also considered valid. 4/5/20
+
+'''
+class Solution:
+  def isValid(self, s):
+    # Fill this in.
+    status = False
+    brace_count = 0
+    curley_count = 0
+
+    for index in s:
+        if index == "("
+
+
+    # Test Program
+    s = "()(){(())"
+    # should return False
+    print(Solution().isValid(s))
+
+    s = ""
+    # should return True
+    print(Solution().isValid(s))
+
+    s = "([{}])()"
+    # should return True
+    print(Solution().isValid(s))
+'''
+
+#Given a sorted array, A, with possibly duplicated elements,
+# find the indices of the first and last occurrences of a
+# target element, x. Return -1 if the target is not found. 4/6/20
+# Input: A = [1,3,3,5,7,8,9,9,9,15], target = 9
+# Output: [6,8]
+#
+# Input: A = [100, 150, 150, 153], target = 150
+# Output: [1,2]
+#
+# Input: A = [1,2,3,4,5,6,10], target = 9
+# Output: [-1, -1]
+
+'''
+class Solution:
+  def getRange(self, arr, target):
+
+
+# Fill this in.
+
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+x = 2
+print(Solution().getRange(arr, x))
+# [1, 4]
+'''
+
+#Given a singly-linked list, reverse the list. This can be done
+# iteratively or recursively. Can you get both solutions? 4/7/20
+# Input: 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+# Output: 0 -> 1 -> 2 -> 3 -> 4 -> NULL
+
+'''
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+  
+  # Function to print the list
+  def printList(self):
+    node = self
+    output = '' 
+    while node != None:
+      output += str(node.val)
+      output += " "
+      node = node.next
+    print(output)
+
+  # Iterative Solution
+  def reverseIteratively(self, head):
+    # Implement this.
+
+  # Recursive Solution      
+  def reverseRecursively(self, head):
+    # Implement this.
+
+# Test Program
+# Initialize the test list: 
+testHead = ListNode(4)
+node1 = ListNode(3)
+testHead.next = node1
+node2 = ListNode(2)
+node1.next = node2
+node3 = ListNode(1)
+node2.next = node3
+testTail = ListNode(0)
+node3.next = testTail
+
+print("Initial list: ")
+testHead.printList()
+# 4 3 2 1 0
+testHead.reverseIteratively(testHead)
+#testHead.reverseRecursively(testHead)
+print("List after reversal: ")
+testTail.printList()
+# 0 1 2 3 4
+'''
+
+#Given a list of numbers with only 3 unique
+# numbers (1, 2, 3), sort the list in O(n) time.
+#Example 1:
+#Input: [3, 3, 2, 1, 3, 2, 1]
+#Output: [1, 1, 2, 2, 3, 3, 3]
+#4/8/20
+
+'''
+def sortNums(nums):
+  # Fill this in.
+
+print sortNums([3, 3, 2, 1, 3, 2, 1])
+# [1, 1, 2, 2, 3, 3, 3]
+'''
+
+#You are given a list of numbers, and a target number k.
+# Return whether or not there are two numbers in the list
+# that add up to k.
+#Example:
+#Given [4, 7, 1 , -3, 2] and k = 5,
+#return true since 4 + 1 = 5.
+#4/9/20
+'''
+def two_sum(list, k):
+  # Fill this in.
+
+print two_sum([4,7,1,-3,2], 5)
+# True
+'''
+
+#Given a list of numbers, where every number shows up
+# twice except for one number, find that one number.
+#Example:
+#Input: [4, 3, 2, 4, 1, 3, 2]
+#Output: 1
+#4/10/20
+
+def singleNumber(nums):
+    num_dict = {nums[0]: 0}
+    index = 0
+
+    for i in range(len(nums)):
+        if nums[i] in num_dict:
+            temp = nums[i]
+            num_dict[temp] = num_dict.get(nums[i]) + 1
+            index = i
+        else:
+            temp = nums[i]
+            num_dict[temp] = 1
+
+    #for key, value in num_dict.items():
+    #    print(key, value)
+    key_list = list(num_dict.keys())
+    value_list = list(num_dict.values())
+
+    print("This number appears only once: ")# + str(key_list[value_list.index(1)]))
+
+    return key_list[value_list.index(1)]
+
+
+
+print("SINGLE NUMBER SHOW-UP")
+print("<-----------------START---------------<")
+print([4, 3, 2, 4, 1, 3, 2])
+print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+#1
+print(">-----------------END--------------->")
+
+
+
+#You are given an array of integers in an arbitrary order.
+# Return whether or not it is possible to make the array
+# non-decreasing by modifying at most 1 element to any value.
+#We define an array is non-decreasing if
+# array[i] <= array[i + 1] holds for every i (1 <= i < n).
+#Example:
+# [13, 4, 7] should return true, since we can modify 13 to any value 4 or less, to make it non-decreasing.
+#[13, 4, 1] however, should return false, since there is no way to modify just one element to make the array non-decreasing.
+#4/11/20
+
+def check(num_array):
+    # Fill this in.
+    status = True
+    flag = 0
+
+    for i in range(len(num_array)):
+
+        if i != (len(num_array) -1):
+            if num_array[i] > num_array[i+1]:
+                flag = flag + 1
+
+    if flag > 1:
+        status = False
+
+    return status
+
+print("INTERGERS IN AN ARBITRARY ORDER")
+print("<-----------------START---------------<")
+print([13, 4, 1])
+print(check([13, 4, 1]))
+# False
+print([1, 4, 7])
+print(check([1, 4, 7]))
+# True
+print([13, 4, 7])
+print(check([13, 4, 7]))
+# True
+print([5,1,3,2,5])
+print(check([5,1,3,2,5]))
+# False
+print(">-----------------END--------------->")
+
+
