126dc109d1947bb58b80aa48e97551390a45a721
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-04-12 12:16:45 -0400

    Added binary tree floor/ceiling  question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -303,4 +303,35 @@ print(check([5,1,3,2,5]))
 # False
 print(">-----------------END--------------->")
 
+#Given an integer k and a binary search tree,
+# find the floor (less than or equal to) of k,
+# and the ceiling (larger than or equal to) of k.
+# If either does not exist, then print them as None.
+# 4/12/20
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+
+def findCeilingFloor(root_node, k, floor=None, ceil=None):
+
+    #Fill this in.
+    root = Node(8)
+    root.left = Node(4)
+    root.right = Node(12)
+
+    root.left.left = Node(2)
+    root.left.right = Node(6)
+
+    root.right.left = Node(10)
+    root.right.right = Node(14)
+
+print("<-----------------START---------------<")
+print("Binary Tree floor and ceiling find")
+#print(findCeilingFloor(root, 5)
+# (4, 6)
+print(">-----------------END--------------->")
 
