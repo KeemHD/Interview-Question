614c19916aa7f24b90aa1c7522e201d2a3c9e567
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-04-13 15:17:45 -0400

    Inverted Binary Tree question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -335,3 +335,43 @@ print("Binary Tree floor and ceiling find")
 # (4, 6)
 print(">-----------------END--------------->")
 
+
+#You are given the root of a binary tree.
+# all left children should become right children,
+# and all right children should become left children.
+# 4/13/20
+# Invert the binary tree in place. That is,
+
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print(self.value)
+    if self.left: self.left.preorder()
+    if self.right: self.right.preorder()
+
+def invert(node):
+    print("")
+  # Fill this in.
+
+
+root = Node('a')
+root.left = Node('b')
+root.right = Node('c')
+root.left.left = Node('d')
+root.left.right = Node('e')
+root.right.left = Node('f')
+
+print("<-----------------START---------------<")
+print("Inverted Binary Tree ")
+root.preorder()
+
+# a b d e c f
+print("\n")
+invert(root)
+root.preorder()
+# a c f b e d
+print(">-----------------END--------------->")
+
