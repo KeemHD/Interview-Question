2554ae5364d43670e8911251777f1b452ad08117
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-04-21 14:54:06 -0400

    Adding Minimum Size Subarray Sum Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,24 @@
+#Given an array of n positive integers and a positive integer s,
+# find the minimal length of a contiguous subarray of which the
+# sum ≥ s. If there isn't one, return 0 instead.
+#Example:
+#Input: s = 7, nums = [2,3,1,2,4,3]
+#Output: 2
+#Explanation: the subarray [4,3]
+# has the minimal length under the problem constraint.
+#4/21/20
+
+class Solution:
+  def minSubArrayLen(self, nums, s):
+    # Fill this in
+    print()
+
+print("Minimum Size Subarray Sum 4-21")
+print("<-----------------START---------------<")
+print(Solution().minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
+# 2
+print("<-----------------END---------------<")
+
 #You are given a 2D array of characters, and a target string.
 # Return whether or not the word target word exists in the matrix.
 # Unlike a standard word search, the word must be either
@@ -23,7 +44,7 @@ def word_search(matrix, word):
             break
 
         h_count = 0
-        v_count =0
+        v_count = 0
 
     return status
 
