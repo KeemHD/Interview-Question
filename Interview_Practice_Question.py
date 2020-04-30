3a2e9e995fe4902f9f2d6227f49129984489b4eb
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-04-30 13:03:34 -0400

    Adding Move Zeros Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,27 @@
+#Given an array nums, write a function to move all 0's
+# to the end of it while maintaining the relative order
+# of the non-zero elements.
+#Example:
+#Input: [0,1,0,3,12]
+#Output: [1,3,12,0,0]
+#You must do this in-place without making a copy of the array.
+#Minimize the total number of operations.
+#4/29/20
+
+class Solution:
+  def moveZeros(self, nums):
+    # Fill this in.
+    print()
+
+nums = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
+Solution().moveZeros(nums)
+print("Course Prerequisites 4-28")
+print("<-----------------START---------------<")
+print(nums)
+# [2, 1, 3, 4, 0, 0, 0, 0, 0, 0]
+print("Move Zeros 4-29")
+print("<-----------------END---------------<")
+
 #You are given a hash table where the key is a course code,
 # and the value is a list of all the course codes that are
 # prerequisites for the key. Return a valid ordering in which
