6a80bed002983c54bd155d2f178d35633f65e89f
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-05-08 15:40:16 -0400

    Adding Create a Balanced Binary Search Tree Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,47 @@
+#Given a sorted list of numbers, change it
+# into a balanced binary search tree.
+# You can assume there will be no duplicate numbers in the list.
+#5/8/20
+
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
+
+  def __str__(self):
+    # level-by-level pretty-printer
+    nodes = deque([self])
+    answer = ''
+    while len(nodes):
+      node = nodes.popleft()
+      if not node:
+        continue
+      answer += str(node.value)
+      nodes.append(node.left)
+      nodes.append(node.right)
+    return answer
+
+
+def createBalancedBST(nums):
+    # Fill this in.
+    print()
+
+print("Create a Balanced Binary Search Tree 5-08")
+print("<-----------------START---------------<")
+print(createBalancedBST([1, 2, 3, 4, 5, 6, 7]))
+# 4261357
+#   4
+#  / \
+# 2   6
+#/ \ / \
+#1 3 5 7
+print("<-----------------END---------------<")
+
+
+
+
+
 #You are given an array of k sorted singly
 # linked lists. Merge the linked lists into
 # a single sorted linked list and return it.
