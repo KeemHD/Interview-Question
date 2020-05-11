b49c641d03863c7ad2a9c208bc5d17facc73a5b9
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-05-11 16:25:31 -0400

    Adding Deepest Node in a Binary Tree Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,43 @@
+#You are given the root of a binary tree. Return the deepest node (the furthest node from the root).
+
+#Example:
+
+#    a
+#   / \
+#  b   c
+# /
+#d
+
+#The deepest node in this tree is d at depth 3.
+#4/11/20
+
+class Node(object):
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+  def __repr__(self):
+    # string representation
+    return self.val
+
+
+def deepest(node):
+    # Fill this in.
+    print()
+
+root = Node('a')
+root.left = Node('b')
+root.left.left = Node('d')
+root.right = Node('c')
+
+print("Deepest Node in a Binary Tree 5-11")
+print("<-----------------START---------------<")
+print(deepest(root))
+# (d, 3)
+print("<-----------------END---------------<")
+
+
 #Given two strings A and B of lowercase letters,
 # return true if and only if we can swap two
 # letters in A so that the result equals B.
