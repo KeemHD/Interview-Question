211c5e38c83a1694c0566f1e00fa6b051edccd36
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-05-12 20:44:12 -0400

    Adding Look and Say Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,29 @@
+#A look-and-say sequence is defined as the integer
+# sequence beginning with a single digit in which
+# the next term is obtained by describing the previous
+# term. An example is easier to understand:
+
+#Each consecutive value describes the prior value.
+
+#1      #
+#11     # one 1's
+#21     # two 1's
+#1211   # one 2, and one 1.
+#111221 # #one 1, one 2, and two 1's.
+
+#Your task is, return the nth term of this sequence.
+
+def lookSay(n):
+    return n
+
+
+print("Look and Say 5-12")
+print("<-----------------START---------------<")
+print(lookSay(input("Enter look/say number: ")))
+# (d, 3)
+print("<-----------------END---------------<")
+
+
 #You are given the root of a binary tree. Return the deepest node (the furthest node from the root).
 
 #Example:
