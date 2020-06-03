4a59a33e5b9f8fdd1489b9c7178ff2649be0a26c
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-03 13:45:38 -0400

    Adding Arithmetic Binary Tree Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,54 @@
+#Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#You are given a binary tree representation
+# of an arithmetic expression. In this tree,
+# each leaf is an integer value,, and
+# a non-leaf node is one of the
+# four operations: '+', '-', '*', or '/'.
+
+#Write a function that takes this tree and evaluates the expression.
+
+#Example:
+
+#    *
+#   / \
+#  +    +
+# / \  / \
+#3  2  4  5
+
+#This is a representation of the expression (3 + 2) * (4 + 5), and should return 45.
+#6/03/20
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+PLUS = "+"
+MINUS = "-"
+TIMES = "*"
+DIVIDE = "/"
+
+def evaluate(root):
+    # Fill this in.
+    print(root)
+
+tree = Node(TIMES)
+tree.left = Node(PLUS)
+tree.left.left = Node(3)
+tree.left.right = Node(2)
+tree.right = Node(PLUS)
+tree.right.left = Node(4)
+tree.right.right = Node(5)
+
+print("Arithmetic Binary Tree 6-03")
+print("<-----------------START---------------<")
+print(evaluate(tree))
+# 45
+print("<-----------------END---------------<")
 #Given a time in the format of hour and minute,
 # calculate the angle of the hour and minute hand on a clock.
 #6/02/20
