a39045fbaa2aa2f8bb9d207789675ecbf2742706
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-07 13:30:39 -0400

    Adding Reverse Integer Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,3 +1,26 @@
+#Hi, here's your problem today. This problem was recently asked by LinkedIn:
+
+#Write a function that reverses the digits a 32-bit signed integer, x. Assume that the environment can only store integers within the 32-bit signed integer range, [-2^31, 2^31 - 1]. The function returns 0 when the reversed integer overflows.
+
+#Example:
+#Input: 123
+#Output: 321
+#6/7/20
+
+class Solution:
+    def reverse(self, x):
+        # Fill this in.
+        print(x)
+
+print("Reverse Integer 6-07")
+print("<-----------------START---------------<")
+print(Solution().reverse(123))
+# 321
+print(Solution().reverse(2**31))
+# 0
+print("<-----------------END---------------<")
+
+
 #Hi, here's your problem today.
 # This problem was recently asked by Twitter:
 
