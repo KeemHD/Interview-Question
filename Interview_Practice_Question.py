d395e1c3e578cfb27d4dd638be9259cdf1752c7f
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-11 11:40:54 -0400

    Adding status = True

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,37 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Facebook:
+
+#Two words can be 'chained' if the last character
+# of the first word is the same as the first
+# character of the second word.
+
+#Given a list of words, determine if there is a
+# way to 'chain' all the words in a circle.
+
+#Example:
+#Input: ['eggs', 'karat', 'apple', 'snack', 'tuna']
+#Output: True
+#Explanation:
+#The words in the order of
+# ['apple', 'eggs', 'snack', 'karat', 'tuna']
+# creates a circle of chained words.
+#6/11/20
+
+from collections import defaultdict
+
+def chainedWords(words):
+    # Fill this in.
+    print(words)
+
+
+print("Circle of Chained Words 6-11")
+print("<-----------------START---------------<")
+print(chainedWords(['apple', 'eggs', 'snack', 'karat', 'tuna']))
+# True
+print("<-----------------END---------------<")
+
+
+#Hi, here's your problem today.
 # This problem was recently asked by Google:
 
 #Given a string with a certain rule:
