a1bab2a1080654003d72fd1a3cbd75499d1eb5e4
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-12 12:39:53 -0400

    Adding Jump to the End Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,33 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Facebook:
 
+#Starting at index 0, for an element n at index i,
+# you are allowed to jump at most n indexes ahead.
+# Given a list of numbers, find the minimum number
+# of jumps to reach the end of the list.
+
+#Example:
+#Input: [3, 2, 5, 1, 1, 9, 3, 4]
+#Output: 2
+#Explanation:
+
+#The minimum number of jumps to get to the end of the list is 2:
+#3 -> 5 -> 4
+#6/12/20
+
+def jumpToEnd(nums):
+    # Fill this in.
+    print(nums)
+
+print("Jump to the End 6-12")
+print("<-----------------START---------------<")
+print(jumpToEnd([3, 2, 5, 1, 1, 9, 3, 4]))
+# 2
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
+# This problem was recently asked by Facebook:
+
 #Two words can be 'chained' if the last character
 # of the first word is the same as the first
 # character of the second word.
@@ -64,6 +91,7 @@ print("<-----------------START---------------<")
 print(chainedWords(['apple', 'eggs', 'snack', 'karat', 'tuna']))
 # True
 print(chainedWords(['eggs', 'karat', 'apple', 'snack', 'tuna']))
+# true
 print("<-----------------END---------------<")
 
 
