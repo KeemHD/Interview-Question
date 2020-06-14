b7905d994dba34c2ca9b302ca81f5faef2b5e253
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-14 00:39:19 -0400

    Adding H-Index Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,34 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
+#The h-index is a metric that attempts to
+# measure the productivity and citation
+# impact of the publication of a scholar.
+# The definition of the h-index is if a scholar
+# has at least h of their papers cited h times.
+
+#Given a list of publications of the number
+# of citations a scholar has, find their h-index.
+
+#Example:
+#Input: [3, 5, 0, 1, 3]
+#Output: 3
+#Explanation:
+#There are 3 publications with 3 or more citations,
+# hence the h-index is 3.
+#6/13/20
+
+def hIndex(publications):
+    # Fill this in.
+    print()
+
+print("H-Index 6-13")
+print("<-----------------START---------------<")
+print(hIndex([5, 3, 3, 1, 0]))
+# 3
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Facebook:
 
 #Starting at index 0, for an element n at index i,
