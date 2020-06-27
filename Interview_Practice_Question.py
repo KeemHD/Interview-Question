d07c82cf8452060f6324d3ce840fb4d9cc581066
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-27 02:06:50 -0400

    Adding Subarray with Target Sum Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,26 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Amazon:
 
+#You are given an array of integers,
+# and an integer K. Return the subarray
+# which sums to K. You can assume that a
+# solution will always exist.
+#Can you do this in linear time?
+#6/26/20
+
+def find_continuous_k(list, k):
+    # Fill this in.
+    print(list,k)
+
+print("Subarray with Target Sum 6-26")
+print("<-----------------START---------------<")
+print(find_continuous_k([1, 3, 2, 5, 7, 2], 14))
+# [2, 5, 7]
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
 #You are given an array of integers.
 # Return the length of the longest consecutive
 # elements sequence in the array.
