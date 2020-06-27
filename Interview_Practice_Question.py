43539bbca18199f2f38abc5d267e685b61cce592
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-27 02:06:05 -0400

    Adding Longest Consecutive Sequence Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,27 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
+#You are given an array of integers.
+# Return the length of the longest consecutive
+# elements sequence in the array.
+
+#For example, the input array [100, 4, 200, 1, 3, 2]
+# has the longest consecutive sequence 1, 2, 3, 4,
+# and thus, you should return its length, 4.
+#Can you do this in linear time?
+#6/25/20
+
+def longest_consecutive(nums):
+    # Fill this in.
+    print(nums)
+
+print("Longest Consecutive Sequence 6-25")
+print("<-----------------START---------------<")
+print(longest_consecutive([100, 4, 200, 1, 3, 2]))
+# 4
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Facebook:
 #6/24/20
 
