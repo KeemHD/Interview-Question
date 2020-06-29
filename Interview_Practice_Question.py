878198493af0c9e34766213cf9cc297d66894271
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-06-29 17:29:02 -0400

    Adding Shifted String Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,25 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#You are given two strings, A and B.
+# Return whether A can be shifted some number of times to get B.
+
+#Eg. A = abcde, B = cdeab should return true because
+# A can be shifted 3 times to the right to get B.
+# A = abc and B= acb should return false.
+#6/29/20
+
+def is_shifted(a, b):
+    # Fill this in.
+    print()
+
+print("Shifted String 6-29")
+print("<-----------------START---------------<")
+print(is_shifted('abcde', 'cdeab'))
+# True
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Microsoft:
 
 #Given the root of a binary tree,
