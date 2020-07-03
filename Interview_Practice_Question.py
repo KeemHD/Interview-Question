c97acf395978580c7ac2a4bd06f76b24b6592202
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-02 23:24:39 -0400

    Adding Binary Tree Level with Minimum Sum Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,43 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Twitter:
+
+#You are given the root of a binary tree.
+# Find the level for the binary tree with
+# the minimum sum, and return that value.
+
+#For instance, in the example below,
+# the sums of the trees are 10, 2 + 8 = 10,
+# and 4 + 1 + 2 = 7. So, the answer here should be 7.
+#7/1/20
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def minimum_level_sum(root):
+    # Fill this in.
+    print()
+
+#     10
+#    /  \
+#   2    8
+#  / \    \
+# 4   1    2
+node = Node(10)
+node.left = Node(2)
+node.right = Node(8)
+node.left.left = Node(4)
+node.left.right = Node(1)
+node.right.right = Node(2)
+
+print("Binary Tree Level with Minimum Sum 7-1")
+print("<-----------------START---------------<")
+print(minimum_level_sum(node))
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Apple:
 
 #You are given the root of a binary tree, along
@@ -18,8 +57,12 @@ class TreeNode:
 
 def lowestCommonAncestor(root, a, b):
     # Fill this in.
-    return root.right
 
+    if a.parent == b.parent:
+        return a.parent
+
+
+    return None
 #   a
 #  / \
 # b   c
