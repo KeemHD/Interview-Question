285536ab375de519dc4b8807fc1dd52f761816f9
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-10 17:13:21 -0400

    Adding Spreadsheet Column Title Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,54 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Amazon:
 
+#MS Excel column titles have the following pattern:
+# A, B, C, ..., Z, AA, AB, ..., AZ, BA, BB, ..., ZZ, AAA,
+# AAB, ... etc. In other words, column 1 is named
+# "A", column 2 is "B", column 26 is "Z", column 27 is "AA"
+# and so forth. Given a positive integer, find its
+# corresponding column name.
+#Examples:
+#Input: 26
+#Output: Z
+
+#Input: 51
+#Output: AY
+
+#Input: 52
+#Output: AZ
+
+#Input: 676
+#Output: YZ
+
+#Input: 702
+#Output: ZZ
+
+#Input: 704
+#Output: AAB
+#7/10/20
+
+class Solution:
+    def convertToTitle(self, n):
+        # Fill this in.
+        print(n)
+
+input1 = 1
+input2 = 456976
+input3 = 28
+print("Spreadsheet Column Title 7-10")
+print("<-----------------START---------------<")
+print(Solution().convertToTitle(input1))
+# A
+print(Solution().convertToTitle(input2))
+# YYYZ
+print(Solution().convertToTitle(input3))
+# AB
+print("<-----------------END---------------<")
+
+
+#Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
 #Version numbers are strings that are used to
 # identify unique states of software products.
 # A version number is in the format a.b.c.d.
