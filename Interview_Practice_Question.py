5e8e3e31308372a5e28e6d96500645fc94c777e6
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-14 16:18:17 -0400

    Adding Print a tree level-by-level, with line-breaks

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,53 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Apple:
 
+#You are given a tree, and your job is to
+# print it level-by-level with linebreaks.
+
+#    a
+#   / \
+#  b   c
+# / \ / \
+#d  e f  g
+
+#The output should be
+#a
+#bc
+#defg
+#7/14/20
+
+from collections import deque
+
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        # Fill this in.
+        return ""
+
+tree = Node('a')
+tree.left = Node('b')
+tree.right = Node('c')
+tree.left.left = Node('d')
+tree.left.right = Node('e')
+tree.right.left = Node('f')
+tree.right.right = Node('g')
+
+print("Print a tree level-by-level, with line-breaks 7-14")
+print("<-----------------START--------------<")
+print(tree)
+# a
+# bc
+# defg
+print("<-----------------END--------------<")
+
+
+#Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
 #In many spreadsheet applications,
 # the columns are marked with letters.
 # From the 1st to the 26th column the
