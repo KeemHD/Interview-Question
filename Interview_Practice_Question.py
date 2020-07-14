6e14c0f2c6a08fc5525acf09db3e75157c9cd2f1
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-14 16:08:34 -0400

    Adding Generate All IP Addresses Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,33 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Microsoft:
 
+#An IP Address is in the format of A.B.C.D,
+# where A, B, C, D are all integers between 0 to 255.
+
+#Given a string of numbers, return the
+# possible IP addresses you can make with that string by
+# splitting into 4 parts of A, B, C, D.
+
+#Keep in mind that integers can't start with a 0! (Except for 0)
+
+#Example:
+#Input: 1592551013
+#Output: ['159.255.101.3', '159.255.10.13']
+#4/12/20
+
+def ip_addresses(s, ip_parts=[]):
+    # Fill this in.
+    print(s)
+
+print("Generate All IP Addresses 7-12")
+print("<-----------------START--------------<")
+print(ip_addresses('1592551013'))
+# ['159.255.101.3', '159.255.10.13']
+print("<-----------------END---------------<")
+
+#Hi, here's your problem today.
+# This problem was recently asked by Microsoft:
+
 #Return the longest run of 1s for a given
 # integer n's binary representation.
 
@@ -26,7 +53,7 @@ def longest_run(n):
         else:
             if count > longest:
                 longest = count
-                count = 0
+            count = 0
 
     return longest
 
