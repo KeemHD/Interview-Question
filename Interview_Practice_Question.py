b7ce95293e78ae3ce1990b4e4111d8c6739d079d
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-14 16:12:54 -0400

    Adding Spreadsheet Columns Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,30 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#In many spreadsheet applications,
+# the columns are marked with letters.
+# From the 1st to the 26th column the
+# letters are A to Z. Then starting from
+# the 27th column it uses AA, AB, ..., ZZ, AAA, etc.
+
+#Given a number n, find the n-th column name.
+#7/13/20
+
+def column_name(n):
+    # Fill this in.
+    print(n)
+
+print("Spreadsheet Columns 7-13")
+print("<-----------------START--------------<")
+print(column_name(26))
+print(column_name(27))
+print(column_name(28))
+# Z
+# AA
+# AB
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Microsoft:
 
 #An IP Address is in the format of A.B.C.D,
