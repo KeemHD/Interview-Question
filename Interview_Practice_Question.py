e7bfd214e7f99cbb7ce2a87b75279df360a4aee9
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-17 20:31:49 -0400

    Adding Most Frequent Subtree Sum Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,38 @@
 #Hi, here's your problem today.
+# This problem was recently asked by LinkedIn:
+
+#Given a binary tree, find the most frequent subtree sum.
+
+#Example:
+
+#   3
+#  / \
+# 1   -3
+
+#The above tree has 3 subtrees. The root node with 3, and the 2 leaf nodes, which gives us a total of 3 subtree sums. The root node has a sum of 1 (3 + 1 + -3), the left leaf node has a sum of 1, and the right leaf node has a sum of -3. Therefore the most frequent subtree sum is 1.
+
+#If there is a tie between the most frequent sum, you can return any one of them.
+#7/17/20
+
+class Node():
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def most_freq_subtree_sum(root):
+    # fill this in.
+    print()
+
+root = Node(3, Node(1), Node(-3))
+
+print("Most Frequent Subtree Sum 7-17")
+print("<-----------------START--------------<")
+print(most_freq_subtree_sum(root))
+# 1
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Twitter:
 
 #Given a string with only ( and ), find the minimum number of characters to add or subtract to fix the string such that the brackets are balanced.
