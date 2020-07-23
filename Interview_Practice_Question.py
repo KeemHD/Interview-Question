d341994a6fd2d95418d89d644310f8b3d293b782
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-23 17:13:53 -0400

    Adding Making a Height Balanced Binary Search Tree

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,47 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Google:
 
+#Given a sorted list, create a height
+# balanced binary search tree, meaning
+# the height differences of each node can only
+# differ by at most 1.
+#7/21/20
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        answer = str(self.value)
+        if self.left:
+            answer += str(self.left)
+        if self.right:
+            answer += str(self.right)
+        return answer
+
+def create_height_balanced_bst(nums):
+    # Fill this in.
+    print(nums)
+
+print("Making a Height Balanced Binary Search Tree 7-21")
+print("<-----------------START--------------<")
+tree = create_height_balanced_bst([1, 2, 3, 4, 5, 6, 7, 8])
+# 53214768
+#  (pre-order traversal)
+#       5
+#      / \
+#     3    7
+#    / \  / \
+#   2   4 6  8
+#  /
+# 1
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
+# This problem was recently asked by Google:
+
 #A chess board is an 8x8 grid. Given a knight
 # at any position (x, y) and a number of moves
 # k, we want to figure out after k random moves
