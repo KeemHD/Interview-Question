b7f77ff15087a46633df0502d22a78cbe2c05c24
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-23 17:08:36 -0400

    Adding Staying on a Chess Board Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,27 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Google:
+
+#A chess board is an 8x8 grid. Given a knight
+# at any position (x, y) and a number of moves
+# k, we want to figure out after k random moves
+# by a knight, the probability that the knight
+# will still be on the chessboard. Once the knight
+# leaves the board it cannot move again and will be
+# considered off the board.
+#7/20/20
+
+def is_knight_on_board(x, y, k, cache={}):
+    # Fill this in.
+    print()
+
+print("Staying on a Chess Board 7-20")
+print("<-----------------START--------------<")
+print(is_knight_on_board(0, 0, 1))
+# 0.25
+print("<-----------------END--------------<")
+
+
+#Hi, here's your problem today.
 # This problem was recently asked by AirBNB:
 
 #We have a list of tasks to perform,
