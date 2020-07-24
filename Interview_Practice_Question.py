8512a04c3a86c8a53006a9e32d07af3b13e011f1
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-24 16:26:14 -0400

    Adding Number of Cousins Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,43 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
+#Given a binary tree and a given node value,
+# return all of the node's cousins. Two nodes are
+# considered cousins if they are on the same
+# level of the tree with different parents.
+# You can assume that all nodes will have their own unique value.
+#7/24/20
+
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def list_cousins(self, tree, val):
+        # Fill this in.
+        print()
+#     1
+#    / \
+#   2   3
+#  / \   \
+# 4   6   5
+root = Node(1)
+root.left = Node(2)
+root.left.left = Node(4)
+root.left.right = Node(6)
+root.right = Node(3)
+root.right.right = Node(5)
+
+print("Number of Cousins 7-24")
+print("<-----------------START--------------<")
+print(Solution().list_cousins(root, 5))
+# [4, 6]
+print("<-----------------END--------------<")
+
+
+#Hi, here's your problem today.
 # This problem was recently asked by Facebook:
 
 #Given a list of building in the form of
