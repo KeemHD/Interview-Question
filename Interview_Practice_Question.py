20d88ca9456167aa0acd78c4907f9ff45bf5658a
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-31 17:56:31 -0400

    Adding Sorting Window Range Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,30 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Twitter:
+
+#Given a list of numbers, find the smallest window
+# to sort such that the whole list will be sorted.
+# If the list is already sorted return (0, 0).
+# You can assume there will be no duplicate numbers.
+
+#Example:
+#Input: [2, 4, 7, 5, 6, 8, 9]
+#Output: (2, 4)
+#Explanation: Sorting the window (2, 4)
+# which is [7, 5, 6] will also means that the whole list is sorted.
+#7/29/30
+
+def min_window_to_sort(nums):
+    # Fill this in.
+    print(nums)
+
+print("Sorting Window Range 7-29")
+print("<-----------------START--------------<")
+print(min_window_to_sort([2, 4, 7, 5, 6, 8, 9]))
+# (2, 4)
+print("<-----------------END--------------<")
+
+
+#Hi, here's your problem today.
 # This problem was recently asked by Apple:
 
 #Given a binary tree, return the list of node
@@ -22,7 +48,9 @@ class Node:
 
 def zigzag_order(tree):
     # Fill this in.
-    print()
+    output = []
+
+    return output
 
 n4 = Node(4)
 n5 = Node(5)
