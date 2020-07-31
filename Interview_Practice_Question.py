3041bc69b4266f1f8be00a697345bf5e96299d3a
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-07-31 18:02:39 -0400

    Adding Rectangle Intersection Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,34 @@
 #Hi, here's your problem today.
+# This problem was recently asked by LinkedIn:
+
+#Given two rectangles, find the area of intersection.
+
+#7/31/20
+
+class Rectangle():
+    def __init__(self, min_x=0, min_y=0, max_x=0, max_y=0):
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+
+def intersection_area(rect1, rect2):
+    # Fill this in.
+    print()
+
+#  BBB
+# AXXB
+# AAA
+rect1 = Rectangle(0, 0, 3, 2)
+rect2 = Rectangle(1, 1, 3, 3)
+
+print("Rectangle Intersection 7-31")
+print("<-----------------START--------------<")
+print(intersection_area(rect1, rect2))
+# 2
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Twitter:
 
 #Given a tree, find if the binary tree is
