3153c1613ad9c49c6c84b745556b7c256d6016e8
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-03 18:00:23 -0400

    Adding Rotate Linked List Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,37 @@
 #Hi, here's your problem today.
+# This problem was recently asked by AirBNB:
+
+#Given a linked list and a number k,
+# rotate the linked list by k places.
+#8/3/20
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        current = self
+        ret = ''
+        while current:
+            ret += str(current.value)
+            current = current.next
+        return ret
+
+def rotate_list(list, k):
+    # Fill this in.
+    print()
+
+# Order is 1, 2, 3, 4
+llist = Node(1, Node(2, Node(3, Node(4))))
+
+print("Rotate Linked List 8-2")
+print("<-----------------START--------------<")
+# Order should now be 3, 4, 1, 2
+print(rotate_list(llist, 2))
+# 3412
+print("<-----------------END--------------<")
+#Hi, here's your problem today.
 # This problem was recently asked by Uber:
 
 #Given a postorder traversal for a binary search tree,
