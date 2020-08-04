4d586abffc134ce9ca595edf5608e9bf1a21e814
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-04 10:52:17 -0400

    Adding Flatten Dictionary Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,51 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Google:
 
+#Given a nested dictionary, flatten the dictionary,
+# where nested dictionary keys can be represented
+# through dot notation.
+
+#Example:
+#Input: {
+#  'a': 1,
+#  'b': {
+#    'c': 2,
+#    'd': {
+#      'e': 3
+#    }
+#  }
+#}
+#Output: {
+#  'a': 1,
+#  'b.c': 2,
+#  'b.d.e': 3
+#}
+#You can assume there will be no arrays,
+# and all keys will be strings.
+#8/4/20
+
+def flatten_dictionary(d):
+    # Fill this in.
+    print(d)
+
+d = {
+    'a': 1,
+    'b': {
+        'c': 2,
+        'd': {
+            'e': 3
+        }
+    }
+}
+print("Flatten Dictionary 8-4")
+print("<-----------------START--------------<")
+print(flatten_dictionary(d))
+# {'a': 1, 'b.c': 2, 'b.d.e': 3}
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
+# This problem was recently asked by Google:
+
 #Given two strings, find if there is a one-to-one
 # mapping of characters between the two strings.
 
@@ -79,7 +124,7 @@ def rotate_list(list, k):
         prev.next = None
         list.next = head
         i+=1
-
+    print(head)
     return list
 
 # Order is 1, 2, 3, 4
