4aa94d8035dbd8bc11e71aa84323dcca3bc11039
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-07 12:04:57 -0400

    Adding Reverse Bits Question

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,4 +1,34 @@
 #Hi, here's your problem today.
+# This problem was recently asked by Amazon:
+
+#Given a 32 bit integer, reverse the bits and return that number.
+
+#Example:
+#Input: 1234
+# In bits this would be 0000 0000 0000 0000 0000 0100 1101 0010
+#Output: 1260388352
+# Reversed bits is 0100 1011 0010 0000 0000 0000 0000 0000
+#8/7/20
+
+def to_bits(n):
+    return '{0:08b}'.format(n)
+
+def reverse_num_bits(num):
+    # Fill this in.
+    print(num)
+    return -1
+
+print("Reverse Bits 8-7")
+print("<-----------------START--------------<")
+print(to_bits(1234))
+# 10011010010
+print(reverse_num_bits(1234))
+# 1260388352
+print(to_bits(reverse_num_bits(1234)))
+# 1001011001000000000000000000000
+print("<-----------------END--------------<")
+
+#Hi, here's your problem today.
 # This problem was recently asked by Facebook:
 
 #Given a list of words, for each word find the
