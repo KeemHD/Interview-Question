f69564078c345a71bb9dd610e30233f1e322889e
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-19 14:43:57 -0400

    Adding Inorder Successor

--- Interview_Practice_Question.py
+++ Interview_Practice_Question.py
@@ -1,6 +1,51 @@
 #Hi, here's your problem today.
 # This problem was recently asked by Google:
 
+#Given a node in a binary search tree
+# (may not be the root), find the next largest
+# node in the binary search tree (also known as an
+# inorder successor). The nodes in this binary search
+# tree will also have a parent field to traverse up the tree.
+#8/19/20
+
+class Node:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+    def __repr__(self):
+        return f"(Value: {self.value}, Left: {self.left}, Right: {self.right})"
+
+def inorder_successor(node):
+    # Fill this in.
+    print()
+
+tree = Node(3)
+tree.left = Node(2)
+tree.right = Node(4)
+tree.left.parent = tree
+tree.right.parent = tree
+tree.left.left = Node(1)
+tree.left.left.parent = tree.left
+tree.right.right = Node(5)
+tree.right.right.parent = tree.right
+#     3
+#    / \
+#   2   4
+#  /     \
+# 1       5
+print("Inorder Successor 8-19")
+print("<-----------------START--------------<")
+print(inorder_successor(tree.left))
+# 3
+print(inorder_successor(tree.right))
+# 5
+print("<-----------------END--------------<")
+#Hi, here's your problem today.
+# This problem was recently asked by Google:
+
 #Given a binary tree, find and return the largest path
 # from root to leaf.
 #8/18/20
